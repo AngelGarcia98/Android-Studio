4f9d041a86b85773ac43e5264b4c0e2cd5fb69b8
AngelGarcia98 <agga0405@gmail.com>
2023-08-01 20:39:11 -0600

    Initial commit

---
+++ app/src/main/java/com/innova/texto_botones/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.innova.texto_botones;
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class MainActivity extends AppCompatActivity{
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
